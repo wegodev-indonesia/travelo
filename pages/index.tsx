c278d5d9fe721c23e2e10625c15b92a37f1fa8a7
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-07-19 14:14:15 +0700

    chore: setup color palletes based on figma design system

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 export default function IndexPage() {
   return (
     <div>
-      <h1 className="text-2xl font-label"> This is a label </h1>
-      <p className="text-2xl">This is paragraph</p>
-      <p className="text-2xl font-mono">This is paragraph mono</p>
-      <p className="text-2xl font-serif">This is paragraph serif</p>
+      <h1 className="text-2xl text-color-primary font-label"> This is a label </h1>
+      <p className="text-2xl text-white">This is paragraph without color</p>
+      <p className="text-2xl text-blue-500 font-mono">This is paragraph mono</p>
+      <p className="text-2xl text-blue-500 font-serif">This is paragraph serif</p>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,51 @@ module.exports = {
         'label': ['Montserrat']
       },
     },
+    colors: {
+      primary: {
+        blue: '#1053B7',
+        white: '#FFFFFF',
+        black: '#050D18'
+      },
+      secondary: {
+        yellow: '#EBA41F',
+        red: '#EB1F2B'
+      },
+      color: {
+        primary: '#050D18',
+        secondary: '#50555D'
+      },
+      white: '#FFFFFF',
+      black: '#000000',
+      yellow: {
+        '100': '#FDF5E8',
+        '200': '#F9E3BB',
+        '300': '#F5D18F',
+        '400': '#F1BF62',
+        '500': '#EBA41F',
+      },
+      red: {
+        '100': '#FDE8E9',
+        '200': '#F9BBBF',
+        '300': '#F58F95',
+        '400': '#F1626A',
+        '500': '#EB1F2B',
+      },
+      blue: {
+        '100': '#E7EDF7',
+        '200': '#B7CBE9',
+        '300': '#87A9DB',
+        '400': '#5786CC',
+        '500': '#1053B7',
+      },
+      gray: {
+        '100': '#E6E6E7',
+        '200': '#B4B6B9',
+        '300': '#82868B',
+        '400': '#50555D',
+        '500': '#050D18',
+      }
+    }
   },
   variants: {
     extend: {},
