58fe4bdaad3ac9dbeb724c9a63ffa6971ff95076
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-07-19 13:58:37 +0700

    chore: install google fonts to nextjs and tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
 export default function IndexPage() {
   return (
-    <div>Hello, World!</div>
+    <div>
+      <h1 className="text-2xl font-label"> This is a label </h1>
+      <p className="text-2xl">This is paragraph</p>
+      <p className="text-2xl font-mono">This is paragraph mono</p>
+      <p className="text-2xl font-serif">This is paragraph serif</p>
+    </div>
   )
 }
