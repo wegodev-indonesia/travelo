1905492cd7e5a30c858e21b594bd8442dc605d06
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-07-21 12:56:36 +0700

    refactor: add props to Container component

--- src/components/common/Container/index.tsx
+++ src/components/common/Container/index.tsx
@@ -2,11 +2,12 @@ import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode
+  size?: string
 }
 
-const Container = ({ children }: Props) => {
+const Container = ({ children, size = 'xl' }: Props) => {
   return (
-    <div className="max-w-screen-xl mx-auto">
+    <div className={`${size === 'xl' ? 'max-w-screen-xl' : 'max-w-screen-lg'} mx-auto`}>
       {children}
     </div>
   );
